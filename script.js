d30151680b21a13b678a3eddf1f43b093e3569cd
Qubit1010 <hassanaleem86@gmail.com>
2023-07-28 19:57:41 +0500

    api link

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('Hello world!')
+// favoriteVoteAPI.js (favoriteVote data retrieval)
+async function fetchFavoriteVoteSegmentData() {
+  try {
+    const response = await fetch(
+      "https://promo-show-backend-default-rtdb.firebaseio.com/dashboard-favorite-calender.json"
+    );
+    if (!response.ok) {
+      throw new Error("Something went wrong!");
+    }
+
+    const favoriteVoteSegmentData = await response.json();
+    return Object.values(favoriteVoteSegmentData);
+  } catch (error) {
+    throw error;
+  }
+}
+
+const favoriteVoteSegmentloadedData = await fetchFavoriteVoteSegmentData();
+console.log(favoriteVoteSegmentloadedData);
